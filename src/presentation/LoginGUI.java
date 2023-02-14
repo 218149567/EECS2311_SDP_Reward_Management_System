dc8b18fc28d5e8b24953924aac82e82e5044777a
donat <donat@DESKTOP-7Q50GOQ>
2023-02-14 01:24:43 -0500

    Changed the size of the frame to accommodate labels,button, field

--- src/presentation/LoginGUI.java
+++ src/presentation/LoginGUI.java
@@ -11,7 +11,7 @@ public class LoginGUI {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(200,200);
+		frame.setSize(400,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		frame.add(panel);	
