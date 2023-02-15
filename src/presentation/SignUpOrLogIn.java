8fa9a69d589d4c698f68ba692168b6cfa5cd489e
farestrad <fares610@my.yorku.ca>
2023-02-15 17:33:25 -0500

    Add ActionListener to the SignupGui in SignUpOrLogin Page

--- src/presentation/SignUpOrLogIn.java
+++ src/presentation/SignUpOrLogIn.java
@@ -1,5 +1,8 @@
 package presentation;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,7 +31,18 @@ public class SignUpOrLogIn {
 		panel.add(logInbutton);
 		
 		
+        signUpbutton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SignupGUI signUpPage = new SignupGUI();
+                signUpPage.show(true);
+                frame.dispose();
+            }
+        });
+		
+		
 		frame.setVisible(true);
+		
 
+		
 	}
 }
