028b98a6454a502ef772c64c1a3a37c07b2b738f
farestrad <fares610@my.yorku.ca>
2023-02-15 11:52:06 -0500

    Delete name box from LoginGUI and resize

--- src/presentation/LoginGUI.java
+++ src/presentation/LoginGUI.java
@@ -11,7 +11,7 @@ public class LoginGUI {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(400,400);
+		frame.setSize(380,350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		frame.add(panel);	
@@ -21,40 +21,40 @@ public class LoginGUI {
 		inputprompt.setBounds(160, 0, 80, 25);
 		panel.add(inputprompt);
 		
-		panel.setLayout(null);
-		JLabel nameofuser = new JLabel("Name");
-		nameofuser.setBounds(10, 30, 80, 25);
-		panel.add(nameofuser);
+//		panel.setLayout(null);
+//		JLabel nameofuser = new JLabel("Name");
+//		nameofuser.setBounds(10, 30, 80, 25);
+//		panel.add(nameofuser);
 		
 		panel.setLayout(null);
-		JLabel Userlabel = new JLabel("Username");
-		Userlabel.setBounds(10, 70, 80, 25);
+		JLabel Userlabel = new JLabel("Username");//10, 30, 80, 25
+		Userlabel.setBounds(10, 30, 80, 25);
 		panel.add(Userlabel);
 		
 		panel.setLayout(null);
 		JLabel Passlabel = new JLabel("Password");
-		Passlabel.setBounds(10, 110, 80, 25);
+		Passlabel.setBounds(10, 70, 80, 25);//10, 70, 80, 25
 		panel.add(Passlabel);
 		
-		JTextField nameText = new JTextField(20);
-		nameText.setBounds(100,30,165,25);
-		panel.add(nameText);
+//		JTextField nameText = new JTextField(20);
+//		nameText.setBounds(100,30,165,25);
+//		panel.add(nameText);
 		
 		JTextField userText = new JTextField(20);
-		userText.setBounds(100,70,165,25);
+		userText.setBounds(100,30,165,25);
 		panel.add(userText);
 		
 		
 		JPasswordField startextpass = new JPasswordField();
-		startextpass.setBounds(100, 110, 165, 25);
+		startextpass.setBounds(100,70,165,25);//100,70,165,25
 		panel.add(startextpass);
 		
 		JButton button = new JButton("Enter");
-		button.setBounds(140, 150, 80, 25);
+		button.setBounds(140, 110, 80, 25);
 		panel.add(button);
 		
 		JButton gobackButton = new JButton("Back");
-		gobackButton.setBounds(140, 180, 80, 25);
+		gobackButton.setBounds(140, 140, 80, 25);
 		panel.add(gobackButton);
 		
 		frame.setVisible(true);
