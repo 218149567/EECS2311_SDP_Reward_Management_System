7fd14a0ec8f6e74f423951fe3fec2704ee5f4b7a
juliensk <kasdapex@my.yorku.ca>
2023-02-17 09:13:37 -0500

    modified test cases

--- test/persistenceTest/StubDataBaseTest.java
+++ test/persistenceTest/StubDataBaseTest.java
@@ -1,18 +1,12 @@
 package persistenceTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import businessLogic.Account;
-import businessLogic.Administrator;
-import businessLogic.PointsReceiver;
-import businessLogic.Reward;
+import businessLogic.*;
 import persistence.StubDataBase;
 
 public class StubDataBaseTest {
