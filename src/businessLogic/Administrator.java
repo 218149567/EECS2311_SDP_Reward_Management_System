9b999325bcdd3ee22a89bf083e29eeff11288ad7
218149567 <stefans3@my.yorku.ca>
2023-02-16 23:16:26 -0500

    Added multiple methods: createAdmin, addPoints, changePassword, and removeAccount

--- src/businessLogic/Administrator.java
+++ src/businessLogic/Administrator.java
@@ -9,4 +9,28 @@ public class Administrator extends Account{
 		super(name, username, password);
 		this.role = "AD";
 	}
+	
+	public void createAdmin(String name, String username, String password) {
+		
+		Administrator admin = new Administrator(name, username, password); 
+		system.addAccount(admin);
+	}
+	
+	public void addPoints(int points, String username){
+		Account user = system.getAccount(username);
+		
+		if (user.getRole().equals("PR")) {
+			((PointsReceiver) user).addPoints(points);
+		}
+		
+	}
+	
+	public void changePassword(String username, String newPassword) {
+		system.getAccount(username).setPassword(newPassword);
+	}
+	
+	public void removeAccount(String username) {
+		int index = system.getDb().getAccounts().indexOf(system.getAccount(username));
+		system.getDb().getAccounts().remove(index);
+	}
 }
