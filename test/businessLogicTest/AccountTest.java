293805ea041f1c13ceb9d250f20b6b9b6ab1a647
juliensk <kasdapex@my.yorku.ca>
2023-02-17 11:45:49 -0500

    Updated log file

--- test/businessLogicTest/AccountTest.java
+++ test/businessLogicTest/AccountTest.java
@@ -3,8 +3,7 @@ import businessLogic.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class AccountTest {
 
