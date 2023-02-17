7dfa56b78fb4b4f84db39f325cfa07a8cc3f1093
juliensk <kasdapex@my.yorku.ca>
2023-02-16 23:11:06 -0500

    changes

---
+++ test/businessLogicTest/AdministratorTest.java
@@ -0,0 +1,14 @@
+package businessLogicTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdministratorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/businessLogicTest/RewardTest.java
@@ -0,0 +1,14 @@
+package businessLogicTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RewardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
