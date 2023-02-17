2589da9bf1a5c75b52912b2dd5f18704dd433fc7
juliensk <kasdapex@my.yorku.ca>
2023-02-17 00:10:37 -0500

    RewardTestCase

--- test/businessLogicTest/RewardTest.java
+++ test/businessLogicTest/RewardTest.java
@@ -1,14 +1,46 @@
 package businessLogicTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import businessLogic.*;
 
 import org.junit.jupiter.api.Test;
 
-class RewardTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+public class RewardTest {
 
+    @Test
+    void testRewardConstructor() {
+        Reward reward = new Reward("Test Reward", "This is a test reward", 10, 5);
+        assertEquals("Test Reward", reward.getName());
+        assertEquals("This is a test reward", reward.getDescription());
+        assertEquals(10, reward.getCost());
+        assertEquals(5, reward.getQuantity());
+    }
+
+    @Test
+    void testSetName() {
+        Reward reward = new Reward("Test Reward", "This is a test reward", 10, 5);
+        reward.setName("New Reward Name");
+        assertEquals("New Reward Name", reward.getName());
+    }
+
+    @Test
+    void testSetDescription() {
+        Reward reward = new Reward("Test Reward", "This is a test reward", 10, 5);
+        reward.setDescription("New reward description");
+        assertEquals("New reward description", reward.getDescription());
+    }
+
+    @Test
+    void testSetCost() {
+        Reward reward = new Reward("Test Reward", "This is a test reward", 10, 5);
+        reward.setCost(20);
+        assertEquals(20, reward.getCost());
+    }
+
+    @Test
+    void testSetQuantity() {
+        Reward reward = new Reward("Test Reward", "This is a test reward", 10, 5);
+        reward.setQuantity(10);
+        assertEquals(10, reward.getQuantity());
+    }
 }
